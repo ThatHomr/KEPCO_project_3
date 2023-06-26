85a01efda64055b48ec99a22f0d7e4c5b9408467
ThatHomr <34852381a@gmail.com>
2023-06-26 12:31:39 +0900

    프로젝트 파일 추가

--- Web_Django/mainapp/models.py
+++ Web_Django/mainapp/models.py
@@ -17,29 +17,6 @@ from django.db.models.fields import TextField
 # - user 확장
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.models import User
-# from django.db.models.fields import FileField
-# - DB에서    FileField의 파생클래스로서 이미지 파일인지 체크
-# from django.db.models.fields import ImageField
-# Create your models here.
-
-# class CustomUser(AbstractUser):
-#     email = models.CharField(max_length=100)
-#     phon = models.CharField(max_length=100)
-#     nickname = models.CharField(max_length=100)
-#     class Meta:
-#         # 실제 사용할 테이블 이름 정의
-#         db_table = "User"
-
-#         # 사용할 앱 이름 정의
-#         app_label = "mainapp"
-
-#         # 외부 데이터베이스에 테이블 존재여부 확인
-#         # - 존재하면 : False
-#         # - 존재하지 않으면 : True
-#         # --> 일반적으로 외부에 테이블을 생성한 후 개발이 진행됨
-#         managed = False
-# class Post(models.Model):
-#     objects = models.Manager()
 
 class User(AbstractUser):
     username = CharField(primary_key=True, max_length=20)
@@ -60,106 +37,6 @@ class User(AbstractUser):
         # - 존재하지 않으면 : True
         # --> 일반적으로 외부에 테이블을 생성한 후 개발이 진행됨
         managed = False
-        
-# class User_img(models.Model):
-#     user_num = IntegerField(primary_key=True)
-#     username = models.ForeignKey(User,
-#                                 to_field="username",
-#                                 db_column="username",
-#                                 on_delete=models.CASCADE)
-#     user_path = CharField(max_length=100)
-#     user_date = DateTimeField(auto_now = True)
-    
-#     class Meta:
-#         db_table = "User_img"
-
-#         app_label = "mainapp"
-
-#         managed = False
-        
-        
-# class Provision(models.Model):
-#     pro_code = CharField(primary_key=True, max_length=20)
-#     username = models.ForeignKey(User,
-#                                 to_field="username",
-#                                 db_column="username",
-#                                 on_delete=models.CASCADE)
-#     pro_name = CharField(max_length=20)
-#     pro_content = CharField(max_length=2000)
-#     pro_essential = CharField(max_length=1)
-    
-    
-#     class Meta:
-#         db_table = "PROVISION"
-
-#         app_label = "mainapp"
-
-#         managed = False
-        
-# class Provision_history(models.Model):
-#     his_num = IntegerField(primary_key=True)
-#     pro_code = models.ForeignKey(Provision,
-#                                 to_field="pro_code",
-#                                 db_column="pro_code",
-#                                 on_delete=models.CASCADE)
-#     username = models.ForeignKey(User,
-#                                 to_field="username",
-#                                 db_column="username",
-#                                 on_delete=models.CASCADE)
-#     his_consent = CharField(max_length=1)
-#     his_date = DateTimeField(auto_now_add = True)
-    
-#     class Meta:
-#         db_table = "PROVISIONHISTORY"
-
-#         app_label = "mainapp"
-
-#         managed = False
-        
-# class Naver_account(models.Model):
-#     naver_id = CharField(primary_key=True, max_length=20)
-#     username = models.ForeignKey(User,
-#                                 to_field="username",
-#                                 db_column="username",
-#                                 on_delete=models.PROTECT)
-#     naver_token = CharField(max_length=255)
-    
-#     class Meta:
-#         db_table = "NAVER_ACCOUNT"
-
-#         app_label = "mainapp"
-
-#         managed = False
-
-# class google_account(models.Model):
-#     google_id = CharField(primary_key=True, max_length=20)
-#     username = models.ForeignKey(User,
-#                                 to_field="username",
-#                                 db_column="username",
-#                                 on_delete=models.PROTECT)
-#     google_token = CharField(max_length=255)
-    
-#     class Meta:
-#         db_table = "GOOGLE_ACCOUNT"
-
-#         app_label = "mainapp"
-
-#         managed = False
-
-# class Kakao_account(models.Model):
-#     kakao_id = CharField(primary_key=True, max_length=20)
-#     username = models.ForeignKey(User,
-#                                 to_field="username",
-#                                 db_column="username",
-#                                 on_delete=models.PROTECT)
-#     kakao_token = CharField(max_length=255)
-    
-#     class Meta:
-#         db_table = "KAKAO_ACCOUNT"
-
-#         app_label = "mainapp"
-
-#         managed = False
 
 class Category(models.Model):
     cate_num = IntegerField(primary_key=True)
